c88798f8ee152eca43c30ea71b13b82379244d7f
FoxXix <phir3fx@gmail.com>
2018-10-24 11:39:10 -0600

    Adding inital code

---
+++ src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/General.java
@@ -0,0 +1,10 @@
+package main.edu.colostate.cs.cs414.ByteMe.banqi.client;
+
+public class General extends Piece{
+
+	General(String name, String color, int rank) {
+		super("General", color, 7);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/User.java
@@ -0,0 +1,5 @@
+package main.edu.colostate.cs.cs414.ByteMe.banqi.client;
+
+public class User {
+
+}
