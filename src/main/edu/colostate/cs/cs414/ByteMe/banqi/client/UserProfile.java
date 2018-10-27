6e1ca151534a83573ad3cdf5f28ccd3d01449861
Stephen Porsche <sporsche@oklahoma-city.cs.colostate.edu>
2018-10-27 15:24:36 -0600

    Started work on logging into the system and creating user profiles

--- src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/UserProfile.java
+++ src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/UserProfile.java
@@ -1,5 +1,25 @@
 package main.edu.colostate.cs.cs414.ByteMe.banqi.client;
 
-public class UserProfile {
+import java.time.LocalDateTime;
 
+public class UserProfile {
+	private String nickname = "";
+	private String email = "";
+	private String password = "";
+	private String joinedDate = null;
+	private int wins = 0;
+	private int losses = 0;
+	private int draws = 0;
+	private int forfeits = 0;
+	
+	public UserProfile(String nickname, String email, String password, String joinedDate) {
+		this.nickname = nickname;
+		this.email = email;
+		this.password = password;
+		this.joinedDate = joinedDate;
+	}
+	
+	public String getUserName() {
+		return nickname;
+	}
 }
