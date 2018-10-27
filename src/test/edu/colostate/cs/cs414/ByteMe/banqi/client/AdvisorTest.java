067c6f1bb67c8dda294852517d46c576cea03a0c
Brian <brianlmart@gmail.com>
2018-10-27 11:33:30 -0600

    Added initial test cases

---
+++ src/test/edu/colostate/cs/cs414/ByteMe/banqi/client/AdvisorTest.java
@@ -0,0 +1,14 @@
+package test.edu.colostate.cs.cs414.ByteMe.banqi.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AdvisorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
