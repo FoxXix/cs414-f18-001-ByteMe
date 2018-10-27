e7dbfb1c5208dd4de599844d7eb2d30ab6baff12
Stephen Porsche <sporsche@oklahoma-city.cs.colostate.edu>
2018-10-27 17:49:35 -0600

    Successfully finds already created user profiles, successfully adds new profile and writes the profile to the profile document.  Still need to write Unit Tests

--- src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/UserProfile.java
+++ src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/UserProfile.java
@@ -12,14 +12,74 @@ public class UserProfile {
 	private int draws = 0;
 	private int forfeits = 0;
 	
-	public UserProfile(String nickname, String email, String password, String joinedDate) {
+	public UserProfile(String nickname, String email, String password, String joinedDate, int wins, int losses, int draws, int forfeits) {
 		this.nickname = nickname;
 		this.email = email;
 		this.password = password;
 		this.joinedDate = joinedDate;
+		this.wins = wins;
+		this.losses = losses;
+		this.draws = draws;
+		this.forfeits = 0;
 	}
 	
 	public String getUserName() {
 		return nickname;
+	}
+	
+	public void setUserName(String newName) {
+		this.nickname = newName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String newPassword) {
+		this.password = newPassword;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String newEmail) {
+		this.email = newEmail;
+	}
+	
+	public String getJoinedDate() {
+		return joinedDate;
+	}
+	
+	public int getWins() {
+		return wins;
+	}
+	
+	public void incrementWins() {
+		wins += 1;
+	}
+	
+	public int getLosses() {
+		return losses;
+	}
+	
+	public void incrementLosses() {
+		losses += 1;
+	}
+	
+	public int getDraws() {
+		return draws;
+	}
+	
+	public void incrementDraws() {
+		draws += 1;
+	}
+	
+	public int getForfeits() {
+		return forfeits;
+	}
+	
+	public void incrementForfeits() {
+		forfeits += 1;
 	}
 }
