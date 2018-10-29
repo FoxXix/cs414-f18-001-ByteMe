b818abc6ab69cbeb0233237efe6bab4e57ec2366
Brian <brianlmart@gmail.com>
2018-10-29 15:13:32 -0600

    Adding random set pieces

--- src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/BanqiGame.java
+++ src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/BanqiGame.java
@@ -1,9 +1,14 @@
 package main.edu.colostate.cs.cs414.ByteMe.banqi.client;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class BanqiGame {
 
 	private Board board;
 	private UserProfile userProfile;
+	private int emptyTiles = 32;
 	
 	public BanqiGame() {
 		board = new Board();
@@ -14,7 +19,8 @@ public class BanqiGame {
 	}
 
 	public void setUpBoard() {
-		// TODO Auto-generated method stub
+		setPieces("Red");
+		setPieces("Black");
 		
 	}
 
@@ -30,6 +36,37 @@ public class BanqiGame {
 
 	public boolean getStateOfBoard() {
 		return false;		
+	}
+	
+	private void setPieces(String color) {
+		List<Piece> pieces = new ArrayList<Piece>();
+		int[] position;
+		
+		position = getRandomEmptyTilePosition();
+		General general = getGeneral(color, position);
+		pieces.add(general);
+		board.getTileInfo(position).setPiece(general);
+		
+	}
+	
+	private int[] getRandomEmptyTilePosition() {
+		if (emptyTiles == 0)
+			return null;
+		Tile t;
+		Random rand = null;
+		int y;
+		int x;
+		do {
+			y = rand.nextInt(8);
+			x = rand.nextInt(4);
+			t = board.getTileInfo(x, y);				
+		} while (t.getPiece() != null);
+		emptyTiles--;
+		return t.getPosition();
+	}
+	
+	private General getGeneral(String color, int[] position) {
+		return new General(color, position[0], position[1]);
 	}
 
 }
