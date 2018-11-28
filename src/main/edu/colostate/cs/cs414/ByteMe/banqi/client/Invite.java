b839f8bd3cedc534e113114f6e5cd850e475cccf
FoxXix <phir3fx@gmail.com>
2018-11-28 10:22:58 -0700

    Creating invite class

--- src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/Invite.java
+++ src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/Invite.java
@@ -1,7 +1,20 @@
 package main.edu.colostate.cs.cs414.ByteMe.banqi.client;
 
+import java.time.LocalTime;
+
 public class Invite {
   
-//To Be Implemented
+	private String id;
+	private LocalTime time;
+	private String status;
+	private User from;
+	private User to;
+	
+	public Invite(User from, User to) {
+		this.from 	= from;
+		this.to		= to;
+		time = LocalTime.now();
+		status = "Open";
+	}
   
 }
