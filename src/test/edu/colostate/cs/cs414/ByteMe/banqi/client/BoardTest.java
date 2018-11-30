3afeba943e2ba1959c3f30c9d7f6531e40e0947f
evansalz <31269464+evansalz@users.noreply.github.com>
2018-11-30 13:10:41 -0700

    Improve Board tests

--- src/test/edu/colostate/cs/cs414/ByteMe/banqi/client/BoardTest.java
+++ src/test/edu/colostate/cs/cs414/ByteMe/banqi/client/BoardTest.java
@@ -2,6 +2,9 @@ package test.edu.colostate.cs.cs414.ByteMe.banqi.client;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
@@ -10,22 +13,34 @@ import main.edu.colostate.cs.cs414.ByteMe.banqi.client.Tile;
 
 class BoardTest {
 
-	static Board board;
-	Tile tile;
-
+	private static Board board;
+	private static Tile tile;
+	private static List boardTiles = new ArrayList<List<Tile>>();
+	private static List row = new ArrayList<List<Tile>>();
+	
 	@BeforeClass
 	public static void initialize(){
 		board = new Board();
+		tile = new Tile(1,1);
+
+		for (int y = 0; y < 8; y++) {
+			row = new ArrayList<Tile>();
+			for (int x = 0; x < 4; x++) {
+				tile = new Tile(x, y);
+				row.add(tile);
+			}
+			boardTiles.add(row);
+		}
     }
 
 	@Test
-	void initializeTest() {
-		assertNotNull(board);
+	void testBoardConstructor() {
+		assertNull(board, "Board Constructor Test");
 	}
 	
 	@Test
-	void getTileInfoTest() {
-		tile = board.getTileInfo(1,1);
-		assertNotNull(tile);
+	void testGetTileInfo() {
+		Tile newTile = board.getTileInfo(1,1);
+		assertEquals(tile, newTile, "Board getTileInfo() test");
 	}
 }
