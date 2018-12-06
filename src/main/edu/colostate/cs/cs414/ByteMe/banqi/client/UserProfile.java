7d16b68fcc5f54fae0aad7cda20de22b4ca77434
Brian <brianlmart@gmail.com>
2018-12-06 11:12:44 -0700

    Forfeits was getting hardcoded to 0 and not being set

--- src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/UserProfile.java
+++ src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/UserProfile.java
@@ -18,7 +18,7 @@ public class UserProfile {
 		this.wins = wins;
 		this.losses = losses;
 		this.draws = draws;
-		this.forfeits = 0;
+		this.forfeits = forfeits;
 	}
 	
 	public String getUserName() {
