cd154a3397202d95f53c7c87624e4f64990223f3
evansalz <31269464+evansalz@users.noreply.github.com>
2018-12-09 19:04:18 -0700

    Update ChariotTest.java

--- src/test/edu/colostate/cs/cs414/ByteMe/banqi/client/ChariotTest.java
+++ src/test/edu/colostate/cs/cs414/ByteMe/banqi/client/ChariotTest.java
@@ -2,28 +2,27 @@ package test.edu.colostate.cs.cs414.ByteMe.banqi.client;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import main.edu.colostate.cs.cs414.ByteMe.banqi.client.Chariot;
 
-class ChariotTest {
-	
-	static Chariot chariot;
+public class ChariotTest {
 
-	@BeforeClass
-	public static void initialize(){
+	private static Chariot chariot, chariotNull;
+	
+	@BeforeAll
+	public static void initialize() {
 		chariot = new Chariot("Red", 1, 1);
-    }
-
-	@Test
-	void initializeTest() {
-		assertNotNull(chariot);
+    	}
+	
+	@Test 
+	public void testNullChariot() throws NullPointerException {
+		assertNull(chariotNull, "Test null Chariot");
 	}
 	
 	@Test
-	void moveTest() {
-		assertTrue(chariot.movePiece());
+	public void testNotNullChariot() throws NullPointerException {
+		assertNotNull(chariot, "Test not null Chariot");
 	}
-
 }
