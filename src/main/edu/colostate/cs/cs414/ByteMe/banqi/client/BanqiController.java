db564cda196fa8b83abea33d447c069ed3b9a5bf
evansalz <31269464+evansalz@users.noreply.github.com>
2018-12-09 18:07:42 -0700

    Add javadoc comments to BanqiController.java

--- src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/BanqiController.java
+++ src/main/edu/colostate/cs/cs414/ByteMe/banqi/client/BanqiController.java
@@ -1,3 +1,9 @@
+/**
+ * This class implements the builder design pattern.  This is not one of the concepts in the domain of the system.
+ * Seemingly, this class is here to separate the creation of objects from their actual representation.
+ * From the client package, most of the functionality of the system happens here or at least runs through here, 
+ * while much less is done in the other classes which are part of the domain.
+ */
 package main.edu.colostate.cs.cs414.ByteMe.banqi.client;
 
 import java.io.BufferedReader;
@@ -27,12 +33,20 @@ public class BanqiController {
 	private boolean isUser = false;
 	// private boolean requestPass = false;
 
+	/**
+	 *
+	 * @param u, a User to be set
+	 */
 	public void setUser(User u) {
 		this.U = u;
 	}
 
 	private boolean validProfile = false;
-	
+
+	/**
+	 * If a User has an assigned valid profile, this sets validProfile to true.
+	 * @param bool, the presence or absence of a UserProfile
+	 */
 	public void setValidProfile(boolean bool) {
 		validProfile = bool;
 	}
@@ -40,46 +54,58 @@ public class BanqiController {
 	//read user inputs
 	private static BufferedReader read;
 
-	// constructor
+	/**
+	 * Constructs a BanqiController with a profile of registered Users.
+	 * @param file, a UserProfiles.txt file containing the data for all of the registered Users in the system.
+	 */
 	public BanqiController(String file) {
 		this.profilesFile = file;
 	}
-
+	
+	/**
+	 * Constructs a BanqiController that connects to a UserNode instance.
+	 * @param usernode, a UserNode connection to the Server for the Banqi Game system.
+	 */
 	public BanqiController(UserNode usernode) {
 		this.usernode = usernode;
 	}
-
+	
+	/**
+	 * @param names, an ArrayList of all of the nickname for registered Users.
+	 */
 	public void setNames(List<String> names) {
 		this.userNames = names;
 
 	}
-
+	
+	/**
+	 * @param board, the board to be set
+	 */
 	public void setBoard(Board board) {
 		this.board = board;
-//		System.out.println(this.board);
-//		System.out.println(board);
 //		game.updateBoard(board);
 	}
 
-	/*
-	 * Reads a file for a User and adds the contents to the list of User Profiles.
-	 * The file contains the unique account details as well as the user's game
-	 * performance. With all of the data from this file, a new User Profile gets
-	 * created.
+	/**
+	 * @return an ArrayList of registered UserProfiles
 	 */
-
 	public List<UserProfile> getListProfiles() {
 		return listOfProfiles;
 	}
-
+	
+	/**
+	 * @return an ArrayList of Users who are registered in the system
+	 */
 	public List<User> getListUsers() {
 		return users;
 	}
 
-	/* Reads a file for a User  and adds the contents to the list of User Profiles.
-	The file contains the unique account details as well as the user's game performance.
-	With all of the data from this file, a new User Profile gets created.
-	*/	
+	/**
+	 * 	Reads a file for a User  and adds the contents to the list of User Profiles.
+	 * 	The file contains the unique account details as well as the user's game performance.
+	 * 	With all of the data from this file, a new User Profile gets created.
+	 * @throws IOException if the profilesFile cannot be read or found.
+	 */
 	public void readUsers() throws IOException {
 
 		FileReader file = new FileReader(profilesFile);
@@ -128,7 +154,7 @@ public class BanqiController {
 
 	}
 
-	/*
+	/**
 	 * The method controls the entrance into and exit from the game system as well
 	 * as registration. While controlling the running of the system, this calls upon
 	 * other methods to do these tasks. It allows for and connects to the following
@@ -136,6 +162,8 @@ public class BanqiController {
 	 * (for new users) [enter '2'] - Log out [enter 'exit'] If the input is not one
 	 * of the previous options, the user is prompted that the input is not
 	 * recognized.
+	 * @throws IOException if the UserProfiles.txt file cannot be read or found.
+	 * @throws InterruptedException if the connection to the Server while running is interrupted
 	 */
 	public void runProgram() throws IOException, InterruptedException {
 		readUsers();
@@ -399,7 +427,7 @@ public class BanqiController {
 		}
 	}
 
-	/*
+	/**
 	 * This method is called from the runProgram method, for when a User enters '1'
 	 * for login, the runProgram method calls this, asking the User to enter their
 	 * nickname. The following checks are done: - If a profile exists with that
@@ -416,7 +444,15 @@ public class BanqiController {
 		usernode.logIn(name, password);
 	}
 
-  public Board updateBoard(List<String[]> pieceNames, List<String[]> pieceColors, List<boolean[]> pieceVis) {
+	/**
+	 * Updates the board of a Banqi Game when first making the board, or whenever one of the Users
+	 * makes a move.
+	 * @param pieceNames, an ArrayList of the Pieces on the Banqi Game Board
+	 * @param pieceColors, an ArrayList of the colors of all the Pieces on the board
+	 * @param pieceVis, an ArrayList stating the visibility of each of the Pieces on the board
+	 * @return Board board, the board of a Banqi Game after any change/move
+	 */
+  	public Board updateBoard(List<String[]> pieceNames, List<String[]> pieceColors, List<boolean[]> pieceVis) {
 		System.out.println("in updateboard");
 		Board board = new Board();
 
@@ -448,14 +484,17 @@ public class BanqiController {
 		return board;
 	}
 
-  	/* This implements the functionality needed to register a new User.
-	The User enters:
-	  - a unique nickname (not in the system)
-	  - an email
-	  - a valid password, entered twice for verification
-	The system takes care:
-	  - date/time of the account creation
-	  - creating the new User Profile */
+  	/** This implements the functionality needed to register a new User.
+	 * The User enters:
+	 * - a unique nickname (not in the system)
+	 * - an email
+	 * - a valid password, entered twice for verification
+	 *The system takes care:
+	 * - date/time of the account creation
+	 * - creating the new User Profile
+	 * @throws IOException if the input from the UserNode has an issue
+	 * @throws InterruptedException if the connection to the Server is interrupted
+	 */
 	private void makeNewUser() throws IOException, InterruptedException {
 		java.io.Console console = System.console();
 		String nickname = "";
@@ -487,9 +526,9 @@ public class BanqiController {
 		} while (!validProfile);
 	}
 
-	/*
-	 * This function returns a user object given the users nickname. Null is
-	 * returned if not found.
+	/**
+	 * @param nickname, the name of the User to get
+	 * @return a User, unless the nickname is not registered with a User, then returns null
 	 */
 	public static User getUser(String nickname) {
 		for (User user : users) {
@@ -500,6 +539,9 @@ public class BanqiController {
 		return null;
 	}
 
+	/**
+	 * @param userStatus, set the Status of the User (i.e. registered)
+	 */
 	public void setUserStatus(boolean userStatus) {
 		this.isUser = userStatus;
 	}
@@ -516,7 +558,11 @@ public class BanqiController {
 		System.out.println(title);
 	}
 
-	//initializes a game, and sets the board as given from the server
+	/**
+	 * Initializes a game, and sets the board as given from the server
+	 * @param opponent, the other User whose turn it is not
+	 * @param turn, true or false, depending on whether it's this user's turn or the other's
+	 */
 	public void startGame(String opponent, boolean turn) {
 		game = new BanqiGame(U, BanqiController.getUser(opponent));
 		game.setReader(read);
@@ -526,19 +572,23 @@ public class BanqiController {
 			game.printBoard();
 		}
 	}
-	
+
+	/**
+	 * Prints a series of lines between printouts in the terminal to make things clearer for the Users
+	 */
 	public void printSpacer() {
 		for (int i = 0; i < 30; i++) {
 			System.out.println();
 		}
 	}
 	
-//	public static void main(String args[]) throws IOException {
-//		BanqiController banqi = new BanqiController(args[0]);
-//		banqi.runProgram();
-//	}
-
-	//makes a Move.  If it is not your turn, the program will wait
+	/**
+	 * Makes a Move.  If it is not a User's turn, the program will wait on that end until the other User plays.
+	 * @param turn, the User who is allowed to move
+	 * @param opponent, the User who is not currently allowed to move
+	 * @param gameID, the unique ID associated with the game
+	 * @throws IOException if there is an error with the UserNode input.
+	 */
 	public void makeMove(boolean turn, String opponent, int gameID) throws IOException {
 		game.updateBoard(board);
 		game.printBoard();
